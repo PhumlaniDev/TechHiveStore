54f1aa75f258a4d730dd3cf19f58615cc4539e9e
Phumlani Arendse <aphumlani.dev@gmail.com>
2024-07-20 17:44:06 +0200

    encrypted sensitive data and secured endpoint

--- src/main/java/com/phumlanidev/techhivestore/TechHiveStoreApplication.java
+++ src/main/java/com/phumlanidev/techhivestore/TechHiveStoreApplication.java
@@ -1,9 +1,11 @@
 package com.phumlanidev.techhivestore;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class TechHiveStoreApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/phumlanidev/techhivestore/config/SecurityConfig.java
+++ src/main/java/com/phumlanidev/techhivestore/config/SecurityConfig.java
@@ -32,11 +32,10 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/auth/register").permitAll()
-                        .requestMatchers("/api/v1/auth/login").permitAll()
-                        .requestMatchers("/api/v1/store").permitAll()
-                        .requestMatchers("/api/v1/admin").hasRole(ADMIN)
-                        .requestMatchers("/api/v1/user").hasRole(USER)
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/store/**").permitAll()
+                        .requestMatchers("/api/v1/admin/**").hasRole(ADMIN)
+                        .requestMatchers("/api/v1/user/**").hasRole(USER)
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
